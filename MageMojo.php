81cebae3e3f8df4ce824767c7f6b9b1e72962205
Rafael Corrêa Gomes <rafaelstz@users.noreply.github.com>
2019-01-03 21:34:14 -0200

    Fix the execution

--- MageMojo.php
+++ MageMojo.php
@@ -13,30 +13,30 @@ set('stratus_cli', '/usr/share/stratus/cli');
 
 desc('It will issue a redeploy of PHP-FPM services');
 task('mm:autoscaling:reinit', function () {
-    run("{{release_path}} autoscaling.reinit");
+    run("{{stratus_cli}} autoscaling.reinit");
 });
 
 desc('Clears everything');
 task('mm:cache:clear', function () {
-    run("{{release_path}} cache.all.clear");
+    run("{{stratus_cli}} cache.all.clear");
 });
 
 desc('Clears Cloudfront cache');
 task('mm:cloudfront:clear', function () {
-    run("{{release_path}} cache.cloudfront.invalidate");
+    run("{{stratus_cli}} cache.cloudfront.invalidate");
 });
 
 desc('Clears OPCache cache');
 task('mm:opcache:clear', function () {
-    run("{{release_path}} cache.opcache.flush");
+    run("{{stratus_cli}} cache.opcache.flush");
 });
 
 desc('Clears Redis cache');
 task('mm:redis:clear', function () {
-    run("{{release_path}} cache.redis.clear");
+    run("{{stratus_cli}} cache.redis.clear");
 });
 
 desc('Clears Varnish cache');
 task('mm:varnish:clear', function () {
-    run("{{release_path}} cache.varnish.clear");
+    run("{{stratus_cli}} cache.varnish.clear");
 });
